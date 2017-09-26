a98d8de4ac9e0b7b2aaf4f102496fa847f31f3c7
Kenneth Damkjær Nielsen <kenn6091@edu.eal.dk>
2017-09-26 12:18:46 +0200

    log

--- LogReader/Program.cs
+++ LogReader/Program.cs
@@ -1,6 +1,26 @@
-﻿namespace LogReader {
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace LogReader {
     class Program {
         static void Main(string[] args) {
+
+            Program Run = new Program();
+            Run.Run();
+        }
+
+        private void Run() {
+            string SavePath = "C:\\Users\\Kenneth\\Desktop\\logfil.txt";
+            var text = File.ReadAllLines(SavePath);
+
+            foreach (var line in text) {
+                Console.WriteLine(line);
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Press 'any' key to exit...");
+            Console.ReadKey();
         }
     }
 }
