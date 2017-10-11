eca45cfdec4d9218b4c2b553f7df80cff67c6e8f
Nikolaj Grill Nielsen <nikolaj@grillnielsen.dk>
2017-10-11 15:33:30 +0200

    f

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LogReader;
 
 namespace UnitTestProject
 {
     [TestClass]
     public class UnitTest1
     {
+        Program p = new Program();
+
         [TestMethod]
         public void TestMethod1()
         {
